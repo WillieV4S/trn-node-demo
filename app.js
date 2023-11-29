c99df13e68b6f924e136fe7dbf02850b4b9d1903
WillieV4S <williebig@gmail.com>
2023-11-29 11:45:59 +0100

    11-29 JAV

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'))
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* Kezdőlap */
 app.get('/', function (req, res) {
@@ -27,7 +27,7 @@ app.get('/', function (req, res) {
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
+  next(createError(404));
 });
 
 // error handler
